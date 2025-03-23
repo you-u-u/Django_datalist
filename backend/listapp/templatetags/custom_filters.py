a2cede306ac1d5ef44e792a33a73e17eea3a09e6
you-u-u <a6cd1f.g@gmail.com>
2025-03-23 22:12:55 +0900

    不要な文言を削除

--- backend/listapp/templatetags/custom_filters.py
+++ backend/listapp/templatetags/custom_filters.py
@@ -1,7 +1,11 @@
 from django import template
 
+# Djangoにカスタムフィルターを登録
 register = template.Library()
 
+# キーが存在すればそのキーの値を返す、なければ０を返す
+# | これはパイプと呼ばれ左の値を右のフィルター関数に渡すことができる
+# Djangoのテンプレートエンジンでは（）を使用して呼び出すことができない
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key, 0)  # デフォルトは0
